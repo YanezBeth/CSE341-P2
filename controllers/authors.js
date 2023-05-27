3409eb6b02759cc375ba11a17e46bcc42967bc64
Beth Yanez <bethyz@byui.edu>
2023-05-26 20:11:16 -0700

    added addition error handling

--- controllers/authors.js
+++ controllers/authors.js
@@ -7,46 +7,41 @@ const router = express.Router();
 Get all authors from the database
 */
 const getAuthors = async (req, res) => {
-  /*const result = await mongodb.getDb().db().collection('authors').find();
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
-    console.error('Error retrieving authors:', error);
-    res.status(500).json({
-      message: 'Unable to retrieve authors'
-    });
-  };*/
-  mongodb.getDb().db().collection('authors').find().toArray((err, result) => {
-    if (err) {
-      res.status(400).json({
-        message: err
-      });
-    }
+  try {
+    const result = await mongodb.getDb().db().collection('authors').find().toArray();
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error retrieving authors from database:', error);
+    res.status(500).json({
+      message: 'Unable to retrieve authors from database'
+    });
+  }
 };
 
 /*
 GET one author by ID
 */
-const oneAuthor = (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Invalid Author ID');
-  }
-  const authorId = new ObjectId(req.params.id);
-  mongodb.getDb().db().collection('authors').find({
-    _id: authorId
-  }).toArray((err, result) => {
-    if (err) {
-      res.status(500).json({
-        message: err
-      });
+const oneAuthor = async (req, res) => {
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json('Invalid Author ID');
     }
+    const authorId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('authors').find({
+      _id: authorId
+    });
+    if (!result) {
+      return res.status(404).json('Author not found');
+    };
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(result[0]);
-  });
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error retrieving author from database:', error);
+    res.status(500).json({
+      message: 'Unable to retrieve author from database'
+    });
+  }
 };
 
 /*
@@ -54,22 +49,28 @@ POST: add a new author
 */
 const addAuthor = async (req, res) => {
   //console.log('addAuthor', req.body);
-  const newAuthor = {
-    authorLastName: req.body.authorLastName,
-    authorFirstName: req.body.authorFirstName,
-    birthdate: req.body.birthdate,
-    email: req.body.website
-  };
+  try {
+    const newAuthor = {
+      authorLastName: req.body.authorLastName,
+      authorFirstName: req.body.authorFirstName,
+      birthdate: req.body.birthdate,
+      email: req.body.website
+    };
 
-  const result = await mongodb.getDb().db().collection('authors').insertOne(newAuthor);
-  if (result.acknowledged) {
-    res.status(201).json({
-      message: 'Author added successfully',
-      authorId: result.insertedId
+    const result = await mongodb.getDb().db().collection('authors').insertOne(newAuthor);
+    if (result.acknowledged) {
+      res.status(201).json({
+        message: 'Author added successfully',
+        authorId: result.insertedId
+      });
+    } else {
+      res.status(500).json('An error occurred. Unable to add author.');
+    }
+  } catch (error) {
+    console.error('Error adding author to database:', error);
+    res.status(500).json({
+      message: 'Unable to add author to database'
     });
-  } else {
-    res.status(500).json(
-      result.error || 'An error occurred. Unable to add author.');
   }
 };
 
@@ -77,31 +78,34 @@ const addAuthor = async (req, res) => {
 PUT route for updating an author 
 */
 const updateAuthor = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Invalid Author ID');
-  }
-  const authorID = new ObjectId(req.params.id);
-  const upAuthor = {
-    authorLastName: req.body.authorLastName,
-    authorFirstName: req.body.authorFirstName,
-    birthdate: req.body.birthdate,
-    email: req.body.website
-  };
-  const result = await mongodb.getDb().db().collection('authors').replaceOne({
-      _id: authorID
-    },
-    //$set: upAuthor
-    upAuthor
-  );
-  console.log('updateAuthor', result);
-  if (result.modifiedCount > 0) {
-    res.status(204).json({
-      message: 'Author updated successfully'
-    });
-  } else {
-    res.status(500).json( //can remove the result.error
-      result.error || 'Unable to update author'
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json('Invalid Author ID');
+    }
+    const authorID = new ObjectId(req.params.id);
+    const upAuthor = {
+      authorLastName: req.body.authorLastName,
+      authorFirstName: req.body.authorFirstName,
+      birthdate: req.body.birthdate,
+      email: req.body.website
+    };
+    const result = await mongodb.getDb().db().collection('authors').replaceOne({
+        _id: authorID
+      },
+      //$set: upAuthor
+      upAuthor
     );
+    //console.log('updateAuthor', result);
+    if (result.modifiedCount > 0) {
+      res.status(204).json({
+        message: 'Author updated successfully'
+      });
+    } else {
+      res.status(404).json('Author not found');
+    }
+  } catch (error) {
+    console.error('Error updating author in database:', error);
+    res.status(500).json('Unable to update author in database');
   }
 };
 
@@ -109,21 +113,24 @@ const updateAuthor = async (req, res) => {
 DELETE route for deleting an author
 */
 const deleteAuthor = async (req, res) => {
-  if (!ObjectId.isValid(req.params.id)) {
-    res.status(400).json('Invalid Author ID');
-  }
-  const deleteAuthorID = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('authors').deleteOne({
-    _id: deleteAuthorID
-  });
-  if (result.deletedCount > 0) {
-    res.status(200).json({
-      message: 'Author deleted successfully'
+  try {
+    if (!ObjectId.isValid(req.params.id)) {
+      return res.status(400).json('Invalid Author ID');
+    }
+    const deleteAuthorID = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('authors').deleteOne({
+      _id: deleteAuthorID
     });
-  } else {
-    res.status(500).json(
-      result.error || 'Author not found'
-    );
+    if (result.deletedCount > 0) {
+      res.status(200).json({
+        message: 'Author deleted successfully'
+      });
+    } else {
+      res.status(404).json('Author not found');
+    }
+  } catch (error) {
+    console.error('Error deleting author from database:', error);
+    res.status(500).json('Unable to delete author from database');
   }
 };
 
--- controllers/titles.js
+++ controllers/titles.js
@@ -6,13 +6,11 @@ const router = express.Router();
 
 const getTitles = async (req, res) => {
   try {
-    const result = await mongodb.getDb().db().collection('titles').find();
-    result.toArray().then((lists) => {
-      res.setHeader('Content-Type', 'application/json');
-      res.status(200).json(lists);
-    });
+    const result = await mongodb.getDb().db().collection('titles').find().toArray();
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
   } catch (error) {
-    console.log(error);
+    console.log('Error retrieving titles from database:', error);
     res.status(500).json({
       message: 'Unable to get titles'
     });
@@ -21,66 +19,89 @@ const getTitles = async (req, res) => {
 
 //GET one title by ID
 const oneTitle = async (req, res) => {
-  const titleId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('titles').find({
-    _id: titleId
-  });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
-};
-
-const addTitle = async (req, res) => {
-  const newTitle = {
-    title: req.body.title,
-    genre: req.body.genre,
-    audience: req.body.audience,
-    publisher: req.body.publisher,
-    isbn: req.body.isbn,
-    authorFirstName: req.body.authorFirstName,
-    authorLastName: req.body.authorLastName,
-  };
-
-  const result = await mongodb.getDb().db().collection('titles').insertOne(newTitle);
-  if (result.acknowledged) {
-    res.status(201).json({
-      message: 'Title added successfully',
-      titleId: result.insertedId
+  try {
+    const titleId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('titles').find({
+      _id: titleId
     });
-  } else {
-    res.status(404).json({
-      message: 'Unable to add title'
+    if (!result) {
+      return res.status(404).json('Title not found');
+    }
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(result);
+  } catch (error) {
+    console.error('Error retrieving title from database:', error);
+    res.status(500).json({
+      message: 'Unable to retrieve title from database'
     });
   }
 };
+
+const addTitle = async (req, res) => {
+  try {
+    const newTitle = {
+      title: req.body.title,
+      genre: req.body.genre,
+      audience: req.body.audience,
+      publisher: req.body.publisher,
+      isbn: req.body.isbn,
+      authorFirstName: req.body.authorFirstName,
+      authorLastName: req.body.authorLastName,
+    };
+
+    const result = await mongodb.getDb().db().collection('titles').insertOne(newTitle);
+    if (result.acknowledged) {
+      res.status(201).json({
+        message: 'Title added successfully',
+        titleId: result.insertedId
+      });
+    } else {
+      res.status(500).json({
+        message: 'An error occurred. Unable to add title'
+      });
+    }
+  } catch (error) {
+    console.error('Error adding title to database:', error);
+    res.status(500).json({
+      message: 'Unable to add title to database'
+    });
+  }
+};
+
 
 /*
 PUT route for updating an author 
 */
 const updateTitle = async (req, res) => {
-  const titleID = new ObjectId(req.params.id);
-  const upTitle = {
-    title: req.body.title,
-    genre: req.body.genre,
-    audience: req.body.audience,
-    publisher: req.body.publisher,
-    isbn: req.body.isbn,
-    authorFirstName: req.body.authorFirstName,
-    authorLastName: req.body.authorLastName,
-  };
-  const result = await mongodb.getDb().db().collection('titles').updateOne({
-    _id: titleID
-  }, {
-    $set: upTitle
-  });
-  if (result.modifiedCount === 1) {
-    res.status(204).json({
-      message: 'Title updated successfully'
+  try {
+    const titleID = new ObjectId(req.params.id);
+    const upTitle = {
+      title: req.body.title,
+      genre: req.body.genre,
+      audience: req.body.audience,
+      publisher: req.body.publisher,
+      isbn: req.body.isbn,
+      authorFirstName: req.body.authorFirstName,
+      authorLastName: req.body.authorLastName,
+    };
+    const result = await mongodb.getDb().db().collection('titles').updateOne({
+      _id: titleID
+    }, {
+      $set: upTitle
     });
-  } else {
-    res.status(404).json({
-      message: 'Title not found'
+    if (result.modifiedCount > 0) {
+      res.status(204).json({
+        message: 'Title updated successfully'
+      });
+    } else {
+      res.status(404).json({
+        message: 'Title not found'
+      });
+    }
+  } catch (error) {
+    console.error('Error updating title in database:', error);
+    res.status(500).json({
+      message: 'Unable to update title in database'
     });
   }
 };
@@ -89,17 +110,24 @@ const updateTitle = async (req, res) => {
 DELETE route for deleting an author
 */
 const deleteTitle = async (req, res) => {
-  const deleteTitleID = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('titles').deleteOne({
-    _id: deleteTitleID
-  });
-  if (result.deletedCount === 1) {
-    res.status(200).json({
-      message: 'Title deleted successfully'
+  try {
+    const deleteTitleID = new ObjectId(req.params.id);
+    const result = await mongodb.getDb().db().collection('titles').deleteOne({
+      _id: deleteTitleID
     });
-  } else {
-    res.status(404).json({
-      message: 'Title not found'
+    if (result.deletedCount > 0) {
+      res.status(200).json({
+        message: 'Title deleted successfully'
+      });
+    } else {
+      res.status(404).json({
+        message: 'Title not found'
+      });
+    }
+  } catch (error) {
+    console.error('Error deleting title from database:', error);
+    res.status(500).json({
+      message: 'Unable to delete title from database'
     });
   }
 };

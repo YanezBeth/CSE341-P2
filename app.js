8600ad4d754dbd0f9543d85c60b92451d47618d6
Beth Yanez <bethyz@byui.edu>
2023-06-03 18:30:32 -0700

    auth0

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const app = express();
 const cors = require('cors');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
+const { auth } = require('express-openid-connect');
+require('dotenv').config();
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
 app
   .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)) //must be first 
@@ -23,7 +34,9 @@ app
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
-  .use('/', require('./convey'));
+  .use('/', require('./convey'))
+  // auth router attaches /login, /logout, and /callback routes to the baseURL
+  .use(auth(config));
 
 //from Brother Birch's code, catch all that keeps it running but logs the error
 process.on('uncaughtException', (err, origin) => {
--- convey/index.js
+++ convey/index.js
@@ -1,7 +1,25 @@
+const { auth } = require('express-openid-connect');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+
+
 const express = require('express');
 const router = express.Router();
 
 router.use('/', require('./swagger'));
+
+// req.isAuthenticated is provided from the auth router
+router.get('/', (req, res) => {
+  console.log(req.oidc.isAuthenticated());
+  res.render('index', { 
+    title: "Express Demo",
+    isAuthenticated: req.oidc.isAuthenticated(),  });
+  //res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+});
+
+
 router.use('/titles', require('./titles'));
 router.use('/authors', require('./authors'));
 

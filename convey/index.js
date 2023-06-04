3e42ca71bfc1597cfac3cc70cc331744e49d5a4f
Beth Yanez <bethyz@byui.edu>
2023-06-03 18:39:14 -0700

    It's WORKING! Auth0, week 7

--- convey/index.js
+++ convey/index.js
@@ -1,25 +1,31 @@
-const { auth } = require('express-openid-connect');
+const {
+  auth
+} = require('express-openid-connect');
 const dotenv = require('dotenv');
 dotenv.config();
 
-
-
+//from https://auth0.com/
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
 
 const express = require('express');
 const router = express.Router();
 
-router.use('/', require('./swagger'));
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+router.use(auth(config));
 
 // req.isAuthenticated is provided from the auth router
-router.get('/', (req, res) => {
-  console.log(req.oidc.isAuthenticated());
-  res.render('index', { 
-    title: "Express Demo",
-    isAuthenticated: req.oidc.isAuthenticated(),  });
-  //res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+router.get('/checkLoginStatus', (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
-
+router.use('/', require('./swagger'));
 router.use('/titles', require('./titles'));
 router.use('/authors', require('./authors'));
 

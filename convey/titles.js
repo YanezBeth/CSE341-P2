60ef7e43614fda2a6b70cd2c340de1c1268476b8
Beth Yanez <bethyz@byui.edu>
2023-06-09 23:01:57 -0700

    protecting title delete endpoint

--- convey/titles.js
+++ convey/titles.js
@@ -4,6 +4,22 @@ const router = express.Router();
 const titlesController = require('../controllers/titles');
 const validation = require('../middlemanware/validate');
 
+const {
+    auth,
+    requiredScopes
+} = require('express-oauth2-jwt-bearer');
+
+// Authorization middleware. When used, the Access Token must
+// exist and be verified against the Auth0 JSON Web Key Set.
+const checkJwt = auth({
+    audience: 'https://yanezproject2library.onrender.com',
+    issuerBaseURL: `https://dev-qsfk08gwjpmuj0b4.us.auth0.com/`,
+    tokenSigningAlg: 'HS256'
+});
+
+const checkScopes = requiredScopes('read:messages');
+
+
 //Get all titles from the database
 router.get('/', titlesController.getTitles);
 //GET by id
@@ -13,7 +29,7 @@ router.post('/', validation.saveTitle, titlesController.addTitle);
 //Update a field with PUT
 router.put('/:id', validation.saveTitle, titlesController.updateTitle);
 //DELETE route
-router.delete('/:id', titlesController.deleteTitle);
+router.delete('/:id', checkJwt, checkScopes, titlesController.deleteTitle);
 
 
 module.exports = router;
